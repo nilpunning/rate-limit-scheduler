bd8eff91c37cf51f56e58890978fa11ab2ffd018
David O'Meara <nilpunning@gmail.com>
2017-09-30 10:21:07 -0500

    Improve poll size.

--- src/rate_limit_scheduler/batching_rate_limited_service.clj
+++ src/rate_limit_scheduler/batching_rate_limited_service.clj
@@ -1,15 +1,30 @@
 (ns rate-limit-scheduler.batching-rate-limited-service
   (:require [rate-limit-scheduler
              [core :as rls]])
-  (:import [java.lang Math]))
+  (:import [java.lang Math System]))
 
 (def init-state
   {; rate limit per minute
    :limit     2400
-   ; remaining in period
-   :remaining 2
    ; seconds until period ends and remaining resets
-   :reset     60})
+   :reset     60
+   ; remaining in period
+   :remaining 1
+
+   :last-updated})
+
+(defn linear-poll-size
+  [limit reset remaining]
+  ; As the limit is approached more requests are made in an attempt
+  ; to get as close to the limit as possible.
+  (int (Math/round ^double (/ (* (/ limit 60) (- 65 reset)) 2))))
+
+(comment
+  (doseq [i (range 61)]
+    (println i (linear-poll-size 60 (- 60 i) 60)))
+
+  (System/currentTimeMillis)
+  )
 
 (defn batching-rate-limited-service []
   (let [state (atom init-state)]
@@ -18,12 +33,15 @@
       (split-predicate [_ req]
         (:webhook req))
       (poll-size [_]
-        (let [{:keys [limit remaining reset]} state]
+        (let [{:keys [limit reset remaining]} state]
           (if (< reset 5)
             0
             (min
-              (int (Math/floor (+ (* (/ limit 2 50) (- 60 reset)) 2)))
+              (linear-poll-size limit reset remaining)
               remaining))))
       (request-batch [_ reqs]
+
         (doseq [req reqs]
           )))))
+
+
